5a61bee69f6337e41ce53fef573562d581312a58
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-02-28 19:15:37 +0000

    Implement softmax function and enhance TimeStep and BehaviourTracer classes
    A softmax function is added to handle zero probabilities in the action distribution. The TimeStep class is expanded to include several new attributes related to action and control information. BehaviourTracer class is also enhanced; it now has methods to add new time steps to the trajectory and to summarize all time steps upon completion of the trajectory.

--- behaviour_tracer.py
+++ behaviour_tracer.py
@@ -1,9 +1,56 @@
 import gymnasium as gym
 import abstract_agent
+import torch
+
+
+def non_zero_softmax(x: torch.Tensor, epsilon=1e-10) -> torch.Tensor:
+    x += epsilon
+    exp_x = torch.exp(x - torch.max(x))  # Subtract max for numerical stability
+    softmax_x = exp_x / torch.sum(exp_x)
+    return softmax_x
 
 
 class TimeStep:
-    pass
+    def __init__(
+            self,
+            obs: torch.Tensor,
+            action: int,
+            action_distribution: torch.Tensor,
+            prior_action: int,
+            prior_action_distribution: torch.Tensor,
+            reward: float,
+            done: bool,
+            info: dict,
+            previous_control_info_sum: float = 0.0,
+    ):
+        self.obs = obs
+        self.action = action
+        self.action_distribution = non_zero_softmax(action_distribution)
+        self.prior_action = prior_action
+        self.prior_action_distribution = non_zero_softmax(prior_action_distribution)
+        self.reward = reward
+        self.done = done
+        self.info = info
+
+        # get control information
+        self.delta_control_info_tensor: torch.Tensor = self.action_distribution * torch.log2(self.action_distribution / self.prior_action_distribution)
+        self.delta_control_info: float = float(torch.sum(self.delta_control_info_tensor))
+        self.control_info_sum = previous_control_info_sum + self.delta_control_info
+
+    def to_dict(self) -> dict:
+        return {
+            'obs': self.obs.tolist(),
+            'action': self.action,
+            'action_distribution': self.action_distribution.tolist(),
+            'prior_action': self.prior_action,
+            'prior_action_distribution': self.prior_action_distribution.tolist(),
+            'reward': self.reward,
+            'done': self.done,
+            'info': self.info,
+            'delta_control_info_tensor': self.delta_control_info_tensor.tolist(),
+            'delta_control_info': self.delta_control_info,
+            'control_info_sum': self.control_info_sum,
+        }
 
 
 class BehaviourTracer:
@@ -17,3 +64,49 @@ class BehaviourTracer:
         self.agent = agent
         self.prior_agent = prior_agent
         self.trajectory: list[TimeStep] = []
+
+    def add_trajectory(
+            self,
+            obs: torch.Tensor,
+            action: int,
+            action_distribution: torch.Tensor,
+            prior_action: int,
+            prior_action_distribution: torch.Tensor,
+            reward: float,
+            done: bool,
+            info: dict,
+            previous_control_info_sum: float = 0.0,
+    ):
+        time_step = TimeStep(
+            obs,
+            action,
+            action_distribution,
+            prior_action,
+            prior_action_distribution,
+            reward,
+            done,
+            info,
+            previous_control_info_sum
+        )
+        self.trajectory.append(time_step)
+
+    def conclude_trajectory(self) -> dict:
+        conclusion = {
+                'obs': [],
+                'action': [],
+                'action_distribution': [],
+                'prior_action': [],
+                'prior_action_distribution': [],
+                'reward': [],
+                'done': [],
+                'info': [],
+                'delta_control_info_tensor': [],
+                'delta_control_info': [],
+                'control_info_sum': [],
+        }
+        for time_step in self.trajectory:
+            dict_time_tep = time_step.to_dict()
+            for key in dict_time_tep.keys():
+                conclusion[key].append(dict_time_tep[key])
+
+        return conclusion
