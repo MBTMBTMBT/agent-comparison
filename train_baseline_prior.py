dca6abbdee342625690598d4df0dbf7a605eb9ea
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-03-12 00:03:21 +0000

    Correct file extension in `train_baseline_prior.py`
    The commit corrects the file extension in the "env_file" paths for "SimpleGridworld" in `train_baseline_prior.py`. The extension was ".txt.txt" and it has been corrected to ".txt", fixing potential issues related to file handling in the training process.

--- train_baseline_prior.py
+++ train_baseline_prior.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
         # },
         # {
         #     "env_type": "SimpleGridworld",
-        #     "env_file": "envs/simple_grid/gridworld-four-rooms-trap-at-doors-13.txt.txt",
+        #     "env_file": "envs/simple_grid/gridworld-four-rooms-trap-at-doors-13.txt",
         #     "cell_size": None,
         #     "obs_size": None,
         #     "agent_position": None,
@@ -177,7 +177,7 @@ if __name__ == "__main__":
         },
         {
             "env_type": "SimpleGridworld",
-            "env_file": "envs/simple_grid/gridworld-four-rooms-trap-at-doors-13.txt.txt",
+            "env_file": "envs/simple_grid/gridworld-four-rooms-trap-at-doors-13.txt",
             "cell_size": None,
             "obs_size": None,
             "agent_position": None,
