f3cc2d899a4d5ee643a59b94d169132f8aae15a4
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-03-18 15:33:35 +0000

    Update training configurations for maze-13 environment
    Revised training settings in train_baseline.py to accommodate the maze-13 environment. Improvements include repeating training environments for increased randomization, fine-tuning the model base name and evaluation frequency, and adjusting total timesteps. These adjustments are aimed at enhancing training efficiency and effectiveness in the new maze-13 environment.

--- train_baseline.py
+++ train_baseline.py
@@ -1,18 +1,25 @@
 if __name__ == "__main__":
     from stable_baselines3 import PPO
-    from configurations import train_env_configurations, test_env_configurations
+    from configurations import *
     from utils import *
     from functools import partial
     from stable_baselines3.common.env_util import DummyVecEnv
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    # repeat the trained envs, this may help increase randomization
+    rep = 16
+    _train_env_configurations = maze13_train
+    train_env_configurations = []
+    for _ in range(rep):
+        train_env_configurations += _train_env_configurations
+
     env_fns = [partial(make_env, config) for config in train_env_configurations]
 
     env = DummyVecEnv(env_fns)
 
     # dir names
-    base_name = "simple-gridworld-ppo"
+    base_name = "simple-gridworld-ppo-maze-13"
     save_dir = "saved-models"
 
     # Create the save directory if it doesn't exist
@@ -20,15 +27,15 @@ if __name__ == "__main__":
 
     # get callbacks
     test_and_log_callback = TestAndLogCallback(
-        test_env_configurations,
+        maze13_test,
         base_name+"-log",
         n_eval_episodes=10,
-        eval_freq=5000,
+        eval_freq=1000,
         deterministic=False,
         render=False,
         verbose=1,
     )
 
     model = PPO("CnnPolicy", env, policy_kwargs={"normalize_images": False}, verbose=1)
-    model.learn(total_timesteps=3000000, callback=[test_and_log_callback], progress_bar=True)
+    model.learn(total_timesteps=500000, callback=[test_and_log_callback], progress_bar=True)
     save_model(model, 0, base_name, save_dir)
