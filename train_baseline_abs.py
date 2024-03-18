3ec934cac2574b1d6b61e598b62b9f8d28d99b94
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-03-18 10:27:42 +0000

    Increase training environment randomization and update frequencies
    The code changes primarily increase the randomization in the training environment by repeating it, and adjust the frequencies of updating the environment and agent. The abstract rate is also set in the environment's callback function. These amendments aim to improve the training process and results.

--- train_baseline_abs.py
+++ train_baseline_abs.py
@@ -7,12 +7,19 @@ if __name__ == "__main__":
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    env_fns = [partial(make_env, config) for config in train_env_configurations] * 10
+    # repeat the trained envs, this may help increase randomization
+    rep = 2
+    _train_env_configurations = train_env_configurations
+    train_env_configurations = []
+    for _ in range(rep):
+        train_env_configurations += _train_env_configurations
+
+    env_fns = [partial(make_env, config) for config in train_env_configurations]
 
     env = DummyVecEnv(env_fns)
 
     # dir names
-    base_name = "simple-gridworld-ppo-abs"
+    base_name = "simple-gridworld-ppo-abs-half"
     save_dir = "saved-models"
 
     # Create the save directory if it doesn't exist
@@ -32,9 +39,10 @@ if __name__ == "__main__":
     update_env_callback = UpdateEnvCallback(
         train_env_configurations,
         num_clusters=45,
-        update_env_freq=2500,
+        update_env_freq=1000,
         update_agent_freq=20000,
         verbose=1,
+        abs_rate=0.5,
     )
 
     model = PPO("CnnPolicy", env, policy_kwargs={"normalize_images": False}, verbose=1)
