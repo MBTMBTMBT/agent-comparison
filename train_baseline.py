62ae6c6d1fd756ab5f2636a66ebaea2c0401fa24
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-03-20 11:51:56 +0000

    Increase environment randomization and decrease timesteps
    The repetition of trained environments is increased from 2 to 4 for greater randomness. Furthermore, total training timesteps have been reduced from 5,000,000 to 2,000,000. These changes aim to provide a more diversified training scenario with a quicker execution speed.

--- train_baseline.py
+++ train_baseline.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # repeat the trained envs, this may help increase randomization
-    rep = 2
+    rep = 4
     _train_env_configurations = train_env_configurations
     train_env_configurations = []
     for _ in range(rep):
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     )
 
     model = PPO("CnnPolicy", env, policy_kwargs={"normalize_images": False}, verbose=1)
-    model.learn(total_timesteps=5000000, callback=[test_and_log_callback], progress_bar=True)
+    model.learn(total_timesteps=2000000, callback=[test_and_log_callback], progress_bar=True)
     save_model(model, 0, base_name, save_dir)
