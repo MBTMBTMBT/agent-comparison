539a3e5e22f10db3ad8606c6b2199bae4246ad93
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-04-10 23:21:48 +0100

    Refactor training parameters and feature extractor in PPO model
    The training parameters and the sampling configuration used in the algorithm have been adjusted to enhance performance and provide better control. Moreover, the code now uses a custom feature extractor instead of a generic one. This update also includes better model naming and saving process, allowing for easier tracking and management of different model versions.

--- train_baseline_with_feature_extractor.py
+++ train_baseline_with_feature_extractor.py
@@ -1,4 +1,4 @@
-from feature_wrapper import FeatureWrapper
+from feature_wrapper import CustomFeatureExtractor
 
 if __name__ == '__main__':
     import numpy as np
@@ -25,14 +25,14 @@ if __name__ == '__main__':
 
     # model configs
     NUM_ACTIONS = 4
-    LATENT_DIMS = 8
-    RECONSTRUCT_SIZE = (72, 72)
+    LATENT_DIMS = 16
+    RECONSTRUCT_SIZE = (96, 96)
     RECONSTRUCT_SCALE = 2
 
     # sampler configs
     SAMPLE_SIZE = 16384
-    SAMPLE_REPLAY_TIME = 4
-    MAX_SAMPLE_STEP = 4096
+    SAMPLE_REPLAY_TIME = 1
+    MAX_SAMPLE_STEP = SAMPLE_SIZE
     VEC_ENV_REPEAT_TIME = 1
 
     # train hyperparams
@@ -45,18 +45,20 @@ if __name__ == '__main__':
     LR = 1e-4
 
     # train configs
-    PRE_TRAIN_STEPS = SAMPLE_SIZE * SAMPLE_REPLAY_TIME * 50
-    SAVE_FREQ = SAMPLE_SIZE * SAMPLE_REPLAY_TIME * 1
+    PRE_TRAIN_STEPS = SAMPLE_SIZE * SAMPLE_REPLAY_TIME // BATCH_SIZE * 1
+    SAVE_FREQ = SAMPLE_SIZE * SAMPLE_REPLAY_TIME // BATCH_SIZE * 1
 
     EPOCHS = 1000
-    NUM_EPOCH_STEPS = MAX_SAMPLE_STEP * 4
+    NUM_STEPS_PER_EPOCH = MAX_SAMPLE_STEP * 4
 
     # eval configs
     NUM_EVAL_EPISODES = 16
     EVAL_FREQ = MAX_SAMPLE_STEP * 1
 
-    session_name = "learn_feature_reconstruct"
+    session_name = "ppo_feature_extractor"
     feature_model_name = 'feature_model_step'
+    baseline_model_name = 'baseline_model'
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     feature_extractor = FeatureNet(NUM_ACTIONS, n_latent_dims=LATENT_DIMS, lr=LR, img_size=RECONSTRUCT_SIZE,
                                    initial_scale_factor=RECONSTRUCT_SCALE, device=device).to(device)
@@ -64,7 +66,7 @@ if __name__ == '__main__':
     if not os.path.isdir(session_name):
         os.makedirs(session_name)
     # Check for the latest saved model
-    latest_checkpoint = find_latest_checkpoint(session_name)
+    latest_checkpoint = find_latest_checkpoint(session_name, feature_model_name)
 
     # load parameters if it has any
     if latest_checkpoint:
@@ -83,9 +85,9 @@ if __name__ == '__main__':
     print("Start pre-training:")
 
     # Initialize the progress bar
-    pbar = tqdm(total=MAX_SAMPLE_STEP, initial=feature_extractor_step_counter, desc="Pre-Train Progress")
+    pbar = tqdm(total=PRE_TRAIN_STEPS, initial=feature_extractor_step_counter, desc="Pre-Train Progress")
 
-    while feature_extractor_step_counter < MAX_SAMPLE_STEP:
+    while feature_extractor_step_counter < PRE_TRAIN_STEPS:
         sampler = RandomSampler()
         envs = [make_env(config) for config in TRAIN_CONFIGS]
         while len(sampler.transition_pairs) < SAMPLE_SIZE:
@@ -112,9 +114,9 @@ if __name__ == '__main__':
                 pbar.update(1)  # Assuming each batch represents a single step, adjust as necessary
 
                 feature_extractor_step_counter += 1
-                if feature_extractor_step_counter >= MAX_SAMPLE_STEP:
+                if feature_extractor_step_counter >= PRE_TRAIN_STEPS:
                     break
-            if feature_extractor_step_counter >= MAX_SAMPLE_STEP:
+            if feature_extractor_step_counter >= PRE_TRAIN_STEPS:
                 break
 
         if feature_extractor_step_counter % SAVE_FREQ == 0:
@@ -129,15 +131,18 @@ if __name__ == '__main__':
         train_env_configurations += _train_env_configurations
 
     env_fns = [partial(make_env, config, SamplerWrapper) for config in train_env_configurations]
-
-    env = FeatureWrapper(DummyVecEnv(env_fns), feature_extractor.phi, device)
+    env = DummyVecEnv(env_fns)
 
     # Check for the latest saved model
-    model_path, epoch_counter, agent_step_counter = find_latest_checkpoint(session_name)
+    model_path, epoch_counter, agent_step_counter = find_newest_model(baseline_model_name, session_name)
 
     # make model
-    model = PPO("MlpPolicy", env, verbose=1)
+    model = PPO("MlpPolicy", env, verbose=1, policy_kwargs={
+        'features_extractor_class': CustomFeatureExtractor,
+        'features_extractor_kwargs': {'feature_extractor': feature_extractor.phi, 'features_dim': LATENT_DIMS, 'device': device}
+    })
     if model_path is not None:
+        print(f"Loading model from {model_path}")
         model.load(model_path)
 
     print("Start Training:")
@@ -145,6 +150,19 @@ if __name__ == '__main__':
     while epoch_counter < EPOCHS:
         # get callbacks
         step_counter_callback = StepCounterCallback(init_counter_val=agent_step_counter)
+        update_feature_extractor_callback = UpdateFeatureExtractorCallback(
+                    feature_extractor,
+                    env_configurations=TRAIN_CONFIGS,
+                    buffer_size_to_train=SAMPLE_SIZE,
+                    replay_times=SAMPLE_REPLAY_TIME,
+                    batch_size=BATCH_SIZE,
+                    verbose=0,
+                    plot_dir=os.path.join(session_name, 'plots'),
+                    device=device,
+                    tb_writer=tb_writer,
+                    counter=feature_extractor_step_counter,
+                    # show_progress_bar=False,
+            )
         test_and_log_callback = TestAndLogCallback(
             EVAL_CONFIGS,
             session_name,
@@ -153,10 +171,21 @@ if __name__ == '__main__':
             start_num_steps=agent_step_counter,
             deterministic=False,
             render=False,
-            verbose=1,
+            verbose=0,
+        )
+
+        model.learn(
+            total_timesteps=NUM_STEPS_PER_EPOCH,
+            callback=[step_counter_callback, update_feature_extractor_callback, test_and_log_callback],
+            progress_bar=True
         )
 
         agent_step_counter = step_counter_callback.step_count
-        save_model(model, num_epoch=epoch_counter, num_step=agent_step_counter, base_name=session_name, save_dir=session_name)
+        feature_extractor_step_counter = update_feature_extractor_callback.counter
 
+        save_model(model, num_epoch=epoch_counter, num_step=agent_step_counter, base_name=baseline_model_name, save_dir=session_name)
+        _save_name = f"{session_name}/{feature_model_name}_{feature_extractor_step_counter}.pth"
+
+        print(f"Model save to {_save_name}")
+        feature_extractor.save(_save_name, epoch_counter, feature_extractor_step_counter, performance)
         epoch_counter += 1
