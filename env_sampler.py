73cfe2a3b4efd805a4b7cd1ce6cb79fdba1c3c90
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-04-10 16:51:52 +0100

    Add SamplerWrapper class to env_sampler.py
    The SamplerWrapper class has been added to the env_sampler.py file. This class essentially serves as a wrapper for the gym environment library. It includes an initialization routine, a step function to append transition pairs and replace the current observation, and a reset function.

--- env_sampler.py
+++ env_sampler.py
@@ -41,3 +41,21 @@ class RandomSampler:
         increased_size = len(self.transition_pairs) - current_size
         new_size = len(self.transition_pairs)
         return increased_size, new_size
+
+
+class SamplerWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super(SamplerWrapper, self).__init__(env)
+        self.transition_pairs: list[tuple[torch.Tensor, torch.Tensor, torch.Tensor]] = []
+        self.current_obs: torch.Tensor or None = None
+
+    def step(self, action):
+        observation, reward, terminated, truncated, info = self.env.step(action)
+        self.transition_pairs.append((self.current_obs, torch.tensor(action), torch.tensor(observation)))
+        self.current_obs = observation
+        return observation, reward, terminated, truncated, info
+
+    def reset(self, **kwargs):
+        observation, info = self.env.reset(**kwargs)
+        self.current_obs = observation
+        return observation, info
