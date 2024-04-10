05c72a86a61f43d4199a15adc2e74510e74cda93
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-04-10 23:21:38 +0100

    Refactor FeatureWrapper, add CustomFeatureExtractor
    FeatureWrapper class has been refactored to improve its functionality.
    Added a new class, CustomFeatureExtractor that inherits from BaseFeaturesExtractor, to encapsulate feature extraction functionality. This has greatly improved organization and clarity of the code. Reconfiguring these two classes has helped in streamlining the feature extraction process using pytorch.

--- feature_wrapper.py
+++ feature_wrapper.py
@@ -1,23 +1,42 @@
 import torch
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.vec_env import VecEnvWrapper
+import gymnasium as gym
+import numpy as np
 
 
-class FeatureWrapper(VecEnvWrapper):
-    def __init__(self, venv, feature_extractor: torch.nn.Module, device=torch.device('cpu')):
-        super(FeatureWrapper, self).__init__(venv)
-        self.feature_extractor = feature_extractor.to(device)
+# class FeatureWrapper(VecEnvWrapper):
+#     def __init__(self, venv, feature_extractor: torch.nn.Module, obs_shape: tuple[int], device=torch.device('cpu')):
+#         super(FeatureWrapper, self).__init__(venv)
+#         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=obs_shape, dtype=np.float32)
+#         self.feature_extractor = feature_extractor.to(device)
+#         self.device = device
+#
+#     def reset(self):
+#         obs = self.venv.reset()
+#         obs = self.process_obs(obs)
+#         return obs
+#
+#     def step_wait(self):
+#         obs, rewards, dones, infos = self.venv.step_wait()
+#         obs = self.process_obs(obs)
+#         return obs, rewards, dones, infos
+#
+#     def process_obs(self, obs):
+#         obs = torch.tensor(obs, device=self.device).float()
+#         with torch.no_grad():
+#             obs = self.feature_extractor(obs).cpu()
+#         return obs
+
+class CustomFeatureExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Box, feature_extractor: torch.nn.Module, features_dim: int, device=torch.device('cpu')):
+        # The output dimensions of your feature extractor
+        super(CustomFeatureExtractor, self).__init__(observation_space, features_dim=features_dim)
+
+        self._feature_extractor = feature_extractor.to(device)
         self.device = device
 
-    def reset(self):
-        obs = self.venv.reset()
-        return self.process_obs(obs)
-
-    def step_wait(self):
-        obs, rewards, dones, infos = self.venv.step_wait()
-        return self.process_obs(obs), rewards, dones, infos
-
-    def process_obs(self, obs):
-        obs = torch.tensor(obs, device=self.device).float()
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        observations = observations.to(self.device).float()
         with torch.no_grad():
-            obs = self.feature_extractor(obs).cpu().numpy()
-        return obs
+            return self._feature_extractor(observations).to(self.device)
