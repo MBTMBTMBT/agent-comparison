5669e90ac5cdd865b643a5ac2c8a866e35dad27e
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-04-10 18:12:46 +0100

    Add FeatureWrapper class
    A new class called FeatureWrapper has been added in the file feature_wrapper.py. This class inherits from VecEnvWrapper and overloads the reset and step_wait methods to apply a provided feature extractor (a PyTorch model) to observations. This change allows for easier integration of custom feature extraction methods in the environment pipeline.

---
+++ feature_wrapper.py
@@ -0,0 +1,24 @@
+import gymnasium as gym
+import torch
+from stable_baselines3.common.vec_env import VecEnvWrapper
+
+
+class FeatureWrapper(VecEnvWrapper):
+    def __init__(self, venv, feature_extractor: torch.nn.Module, device=torch.device('cpu')):
+        super(FeatureWrapper, self).__init__(venv)
+        self.feature_extractor = feature_extractor.to(device)
+        self.device = device
+
+    def reset(self):
+        obs = self.venv.reset()
+        return self.process_obs(obs)
+
+    def step_wait(self):
+        obs, rewards, dones, infos = self.venv.step_wait()
+        return self.process_obs(obs), rewards, dones, infos
+
+    def process_obs(self, obs):
+        obs = torch.tensor(obs, device=self.device).float()
+        with torch.no_grad():
+            obs = self.feature_extractor(obs).cpu().numpy()
+        return obs
