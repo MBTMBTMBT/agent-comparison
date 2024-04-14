3d090250384c95466bd98e299344cbc33b0dd3b4
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-04-14 20:10:01 +0100

    Update data structures and methods for optimal policy calculation
    TransitionBuffer and RandomSampler classes have been updated to accommodate the computation of the optimal policies. The changes include an addition of an optional fifth element to the transition tuple to store the comparison result between optimal and next optimal. The 'sample' method in RandomSampler has also been modified to compute the optimal policy when requested.

--- env_sampler.py
+++ env_sampler.py
@@ -4,30 +4,43 @@ from torch.utils.data import Dataset
 
 
 class TransitionBuffer(Dataset):
-    def __init__(self, transition_pairs: list[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]]):
+    def __init__(self, transition_pairs: list[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]] or list[
+        tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]]):
         self.transition_pairs = transition_pairs
 
     def __len__(self):
         return len(self.transition_pairs)
 
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor] or list[
+        tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]]:
         return self.transition_pairs[idx]
 
 
 class RandomSampler:
     def __init__(self) -> None:
-        self.transition_pairs: list[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]] = []
+        self.transition_pairs: list[tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]] or list[
+            tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]] = []
 
-    def sample(self, env: gym.Env, max_step=4096) -> tuple[int, int]:
+    def sample(self, env: gym.Env, max_step=4096, compute_optimal=False) -> tuple[int, int]:
         current_size = len(self.transition_pairs)
-        obs, info = env.reset()
+        if compute_optimal:
+            obs, info = env.reset(compute_optimal_policy=compute_optimal)
+        else:
+            obs, info = env.reset()
+        optimal = info['optimal_policy']
         done = False
         reward_sum = 0
         while not done:
             action = env.action_space.sample()
             if action is not None:
                 next_obs, reward, terminated, truncated, info = env.step(action)
-                self.transition_pairs.append((obs, torch.tensor(action), torch.tensor(next_obs), torch.tensor(reward),))
+                if compute_optimal:
+                    next_optimal = info['optimal_policy']
+                    self.transition_pairs.append(
+                        (obs, torch.tensor(action), torch.tensor(next_obs), torch.tensor(reward), torch.tensor(int(optimal == next_optimal))))
+                else:
+                    self.transition_pairs.append(
+                        (obs, torch.tensor(action), torch.tensor(next_obs), torch.tensor(reward),))
                 obs = next_obs
                 reward_sum += reward
                 if terminated:
@@ -52,7 +65,8 @@ class SamplerWrapper(gym.Wrapper):
 
     def step(self, action):
         next_obs, reward, terminated, truncated, info = self.env.step(action)
-        self.transition_pairs.append((self.current_obs, torch.tensor(action), torch.tensor(next_obs), torch.tensor(reward),))
+        self.transition_pairs.append(
+            (self.current_obs, torch.tensor(action), torch.tensor(next_obs), torch.tensor(reward),))
         self.current_obs = next_obs
         return next_obs, reward, terminated, truncated, info
 
