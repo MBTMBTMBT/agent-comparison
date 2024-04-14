353e94227d49333ca83ddcc4137aab16d8b545de
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-04-14 20:41:39 +0100

    Refactor plotting and visualisation in training script
    The plotting and visualisation of both decoded images and model representation during training has been streamlined. The bloated, redundant code in the main training loop has been replaced with dedicated functions, improving readability and maintainability. The batch size also underwent adjustment for optimal performance.

--- train_feature_model.py
+++ train_feature_model.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         'rwd': 0.0,
         'demo': 1.0,
     }
-    BATCH_SIZE = 256
+    BATCH_SIZE = 32
     LR = 1e-4
 
     # train configs
@@ -40,10 +40,12 @@ if __name__ == '__main__':
     TEST_FREQ = 5
 
     session_name = "learn_feature_maze13_demo"
+    feature_model_name = 'feature_model_step'
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = FeatureNet(NUM_ACTIONS, n_latent_dims=LATENT_DIMS, lr=LR, img_size=RECONSTRUCT_SIZE, initial_scale_factor=RECONSTRUCT_SCALE, device=device, weights=WEIGHTS).to(device)
 
-    from utils import find_latest_checkpoint
+    from utils import find_latest_checkpoint, plot_decoded_images, plot_representations
     import os
 
     if not os.path.isdir(session_name):
@@ -112,117 +114,19 @@ if __name__ == '__main__':
                     step_counter += 1
 
         if epoch_counter % TEST_FREQ == 0:
-            encoder = model.phi
-            encoder.eval()
-            decoder = model.decoder
-            decoder.eval()
+            _plot_dir = os.path.join(session_name, 'plots')
             for config, env in zip(CONFIGS, envs):
                 env_path = config['env_file']
                 env_name = env_path.split('/')[-1].split('.')[0]
-                # reset iterator before using it
-                env.iter_reset()
-                # Store z vectors from all iterations
-                z_vectors = []
-                fake_x_imgs = []
-                real_x_imgs = []
-                for observation, terminated, position, connections, reward in env:
-                    if observation is not None:
-                        observation = torch.unsqueeze(observation, dim=0).to(device)
-                        with torch.no_grad():
-                            z = encoder(observation)
-                            fake_x = decoder(z)
-                            z = z.detach().cpu().numpy()
-                            fake_x = fake_x.detach().cpu().numpy()
-                            z_vectors.append(z.squeeze(0))
-                            fake_x_imgs.append(fake_x.squeeze(0))
-                            real_x = observation.detach().cpu().numpy()
-                            real_x_imgs.append(real_x.squeeze(0))
+                if not os.path.isdir(_plot_dir):
+                    os.makedirs(_plot_dir)
+                save_path = os.path.join(_plot_dir, f"{env_name}-{step_counter}.png")
 
-                # plot reconstructed xs:
-                # cannot be used if not using any reconstruction!!!
-                plt.figure()
-                num_xs = len(fake_x_imgs)
-                grid_size = math.ceil(math.sqrt(num_xs))  # Calculate grid size that's as square as possible
-                # Create a figure to hold the grid
-                fig, axes = plt.subplots(grid_size, grid_size, figsize=(grid_size * 2, grid_size * 2), dpi=100)
-                # Flatten axes array for easier indexing
-                axes = axes.ravel()
-                for i, img in enumerate(fake_x_imgs):
-                    # Transpose the image from [channels, height, width] to [height, width, channels] for plotting
-                    img_transposed = img.transpose((1, 2, 0))
-                    image_clipped = np.clip(img_transposed, 0, 1)
-                    # Plot the image in its subplot
-                    axes[i].imshow(image_clipped)
-                    axes[i].axis('off')  # Hide the axis
-                # Hide any unused subplots if the number of images is not a perfect square
-                for j in range(i + 1, grid_size ** 2):
-                    axes[j].axis('off')
-                plt.tight_layout()
-                img_save_dir = os.path.join(session_name, "saved_decoded_images")
-                if not os.path.isdir(img_save_dir):
-                    os.makedirs(img_save_dir)
-                save_path = os.path.join(img_save_dir, f"{env_name}_reconstructed_{epoch_counter}.png")
-                plt.savefig(save_path, dpi=100, bbox_inches='tight')
-                plt.close(fig)
-                # save original images as well
-                plt.figure()
-                # Create a figure to hold the grid
-                fig, axes = plt.subplots(grid_size, grid_size, figsize=(grid_size * 2, grid_size * 2), dpi=100)
-                # Flatten axes array for easier indexing
-                axes = axes.ravel()
-                for i, img in enumerate(real_x_imgs):
-                    # Transpose the image from [channels, height, width] to [height, width, channels] for plotting
-                    img_transposed = img.transpose((1, 2, 0))
-                    image_clipped = np.clip(img_transposed, 0, 1)
-                    # Plot the image in its subplot
-                    axes[i].imshow(image_clipped)
-                    axes[i].axis('off')  # Hide the axis
-                # Hide any unused subplots if the number of images is not a perfect square
-                for j in range(i + 1, grid_size ** 2):
-                    axes[j].axis('off')
-                plt.tight_layout()
-                img_save_dir = os.path.join(session_name, "saved_decoded_images")
-                if not os.path.isdir(img_save_dir):
-                    os.makedirs(img_save_dir)
-                save_path = os.path.join(img_save_dir, f"{env_name}_original_{epoch_counter}.png")
-                plt.savefig(save_path, dpi=100, bbox_inches='tight')
-                plt.close(fig)
-
-                # plot z vectors based on LATENT_DIMS
-                if LATENT_DIMS == 2:
-                    plt.figure(figsize=(8, 8))
-                    for z in z_vectors:
-                        plt.scatter(z[0], z[1])
-                    plt.xlabel("Dimension 1")
-                    plt.ylabel("Dimension 2")
-                elif LATENT_DIMS == 3:
-                    fig = plt.figure(figsize=(8, 8))
-                    ax = fig.add_subplot(111, projection='3d')
-                    for z in z_vectors:
-                        ax.scatter(z[0], z[1], z[2])
-                    ax.set_xlabel("Dimension 1")
-                    ax.set_ylabel("Dimension 2")
-                    ax.set_zlabel("Dimension 3")
-
-                    # different view directions
-                    views = [(30, 30), (30, 60), (30, 90), (60, 30), (60, 60), (90, 90),]  # List of (elev, azim) pairs
-
-                    # Save the plot
-                    img_save_dir = os.path.join(session_name, "saved_encoded_images")
-                    if not os.path.isdir(img_save_dir):
-                        os.makedirs(img_save_dir)
-
-                    for i, (elev, azim) in enumerate(views, start=1):
-                        ax.view_init(elev=elev, azim=azim)
-                        plt.draw()  # Update the plot with the new view
-
-                        # Save each view to a different file
-                        save_path = os.path.join(img_save_dir,
-                                                 f"{env_name}_latent{LATENT_DIMS}_{epoch_counter}_view{i}.png")
-                        plt.savefig(save_path)
-                        # print(f"Saved plot to {save_path}")
-
-                    plt.close(fig)  # Close the plot figure after saving all views
+                if model.decoder is not None:
+                    plot_decoded_images(env, model.phi,
+                                        model.decoder, save_path, device)
+                if LATENT_DIMS == 2 or LATENT_DIMS == 3:
+                    plot_representations(env, model.phi, LATENT_DIMS, save_path, device)
 
         if epoch_counter % SAVE_FREQ == 0:
             model.save(f"{session_name}/model_epoch_{epoch_counter}.pth", epoch_counter, step_counter, performance)
